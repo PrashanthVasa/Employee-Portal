96ecaa511ca8385b7fb74ed3ebe5b75234694daf
PrashanthVasa <prashanthvasa55@gmail.com>
2019-01-18 01:21:07 +0530

    Done with CRUD operations

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyD02V_emePQKmmBFI8zukIIyv4JmHGx38w",
+    authDomain: "vuefsdb.firebaseapp.com",
+    databaseURL: "https://vuefsdb.firebaseio.com",
+    projectId: "vuefsdb",
+    storageBucket: "vuefsdb.appspot.com",
+    messagingSenderId: "501176584678"
+}
